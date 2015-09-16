39cc7b2b8521294d96d5d76b08773abe3739dd54
Justin Niessner <justin.niessner@adroll.com>
2015-09-15 20:46:49 -0400

    Modify the adapter to only catch 400 and 422 responses as model validation errors

--- addon/adapters/web-api.js
+++ addon/adapters/web-api.js
@@ -1,9 +1,17 @@
 import DS from 'ember-data';
 
+const VALIDATION_ERROR_STATUSES = [400, 422];
+
 export default DS.RESTAdapter.extend({
   namespace: 'api',
 
-  ajaxError: function(_, response) {
+  ajaxError: function(xhr, response) {
+    let error = this._super(xhr);
+    
+    if(!error || VALIDATION_ERROR_STATUSES.indexOf(error.status) < 0) {
+      return error;
+    }
+    
     return new DS.InvalidError(JSON.parse(response));
   }
 });
