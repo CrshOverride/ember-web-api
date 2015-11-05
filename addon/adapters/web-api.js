eb612922c0905207fbe80bbd0af382e032f531fe
Justin Niessner <CrshOverride@gmail.com>
2015-11-04 21:55:19 -0500

    Fix style issues

--- addon/adapters/web-api.js
+++ addon/adapters/web-api.js
@@ -1,12 +1,11 @@
 import DS from 'ember-data';
-import Ember from 'ember';
 
 const VALIDATION_ERROR_STATUSES = [400, 422];
 
 export default DS.RESTAdapter.extend({
   namespace: 'api',
 
-  isInvalid: function(status, headers, payload) {
+  isInvalid: function(status) {
     return VALIDATION_ERROR_STATUSES.indexOf(status) >= 0;
   }
 });
